bef8e42424e4456da18a1d6de37dbedf3166d6d6
Slavik Shynkarenko <slavik@slavikdev.com>
2018-06-29 16:19:12 +0200

    Added tests and logic for response object.

--- lib/aquaman/response.rb
+++ lib/aquaman/response.rb
@@ -1,7 +1,21 @@
 module Aquaman
-  # HTTP response.
+  # Generic HTTP response.
   class Response
     def initialize(status, headers, body)
+      @status = status
+      @headers = headers
+      @body = body
+      freeze
+    end
+
+    attr_reader :status, :headers, :body
+
+    def successful?
+      !failed?
+    end
+
+    def failed?
+      status.between?(400, 600)
     end
   end
 end
---
+++ spec/lib/response_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+require_relative 'shared_context'
+
+RSpec.describe Aquaman::Response do
+  include_context 'shared'
+
+  subject(:response) { described_class.new(status, headers, body) }
+
+  let(:status) { 200 }
+  let(:headers) { http_headers }
+  let(:body) { request_body }
+
+  describe '#status' do
+    it { expect(response.status).to eq(status) }
+  end
+
+  describe '#headers' do
+    it { expect(response.headers).to include(headers) }
+  end
+
+  describe '#body' do
+    it { expect(response.body).to eq(body) }
+  end
+
+  describe '#successful?' do
+    context 'when status is not within 400..600 range' do
+      it { expect(response).to be_successful }
+    end
+
+    context 'when status is within 400..600 range' do
+      let(:status) { 500 }
+
+      it { expect(response).not_to be_successful }
+    end
+  end
+
+  describe '#failed?' do
+    context 'when status is within 400..600 range' do
+      let(:status) { 404 }
+
+      it { expect(response).to be_failed }
+    end
+
+    context 'when status is not within 400..600 range' do
+      it { expect(response).not_to be_failed }
+    end
+  end
+end
