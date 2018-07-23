3f064edf06e125985d375d7a4109422e121d852a
Slavik Shynkarenko <slavik@slavikdev.com>
2018-07-23 15:39:29 +0200

    Added tests for acceptable response headers.

---
+++ spec/lib/http/json/acceptable_response_headers_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+require_relative './json_shared_context'
+
+RSpec.describe Aquaman::HTTP::JSON::AcceptableResponseHeaders do
+  include_context 'http-json'
+
+  subject(:headers) { described_class.new(provider_response.response_headers) }
+
+  let(:provider_response) { valid_provider_response }
+
+  describe '.new' do
+    subject(:create) { headers }
+
+    context 'when provider headers have valid content type' do
+      it 'does not raise error' do
+        expect { create }.not_to raise_error
+      end
+    end
+
+    context 'when provider headers have unsupported content type' do
+      let(:provider_response) { unsupported_content_type_provider_response }
+
+      it 'raises UnsupportedContentTypeError' do
+        expect { create }.to raise_error(Aquaman::HTTP::JSON::Errors::UnsupportedContentTypeError)
+      end
+    end
+  end
+end
