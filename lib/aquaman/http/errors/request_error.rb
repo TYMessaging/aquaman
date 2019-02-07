ea2fd5bd4c240e59f05946123803eb3ca3428843
Slavik Shynkarenko <slavik@slavikdev.com>
2019-02-07 17:45:13 +0100

    Expose inner error.

--- lib/aquaman/http/errors/request_error.rb
+++ lib/aquaman/http/errors/request_error.rb
@@ -2,9 +2,12 @@ module Aquaman::HTTP::Errors
   # High-level request error.
   class RequestError < Aquaman::Errors::Error
     def initialize(request, inner_error)
+      @inner_error = inner_error
       super(
         "The following request failed:\n#{request}\n\nError:\n#{inner_error}"
       )
     end
+
+    attr_reader :inner_error
   end
 end
